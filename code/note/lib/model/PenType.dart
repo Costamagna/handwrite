b33568aac2268090d739178eab68fb237d050660
Matteo Costamagna <matteo.costamagna00@gmail.com>
2022-06-07 15:05:15 +0200

    Changed folder

--- code/note/lib/model/PenType.dart
+++
@@ -1,1 +0,0 @@
-enum PenType { pen, selection, rubber }
--- code/note/lib/model/Pens/Pen.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:note/model/penInfo.dart';
-import 'package:note/service/dataService.dart';
-
-abstract class Pen {
-  void onPanStart(DataService data, Offset point, PenInfo penInfo);
-  void onPanUpdate(DataService data, Offset point);
-  void onPanEnd(DataService data);
-}
